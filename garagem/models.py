c68690b782900d975a25b6029574a9bf7583f377
eugreg <daiaegregory@gamail.com>
2023-03-13 14:39:18 -0300

    mudanca na Marca

--- garagem/models.py
+++ garagem/models.py
@@ -4,7 +4,7 @@ class Marca(models.Model):
     nome = models.CharField(max_length=50)
     nacionalidade = models.CharField(max_length=50, null=True, blank=True)
     def __str__(self):
-        return self.nome.upper()
+        return f"{self.nome.upper()}-{self.nacionalidade.upper()}"
     
 class Categoria(models.Model):
     descricao = models.CharField(max_length=100)
