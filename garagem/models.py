837f3283c9a6dba7d366ce19e12c5b97f4f161ab
eugreg <daiaegregory@gamail.com>
2023-03-13 14:18:45 -0300

    Criacao da tabela de marca

---
+++ garagem/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Marca(models.Model):
+    nome = models.CharField(max_length=50)
+    nacionalidade = models.CharField(max_length=50, null=true, blank=true)
+    def __str__(self):
+        return self.nome.upper()
+    
+# Create your models here.
