72a85f2d88b6690d0548c9e72ef0e88ce045d6c3
eugreg <daiaegregory@gmail.com>
2023-03-28 16:36:53 -0300

    acessorio

--- garagem/admin.py
+++ garagem/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from garagem.models import Marca, Categoria,Veiculos, Cor
+from garagem.models import Marca, Categoria,Veiculos, Cor, Acessorio
 
 admin.site.register(Categoria)
 admin.site.register(Marca)
 admin.site.register(Veiculos)
 admin.site.register(Cor)
+admin.site.register(Acessorio)
 
