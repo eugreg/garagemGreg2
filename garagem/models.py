fa7680d5231224f50e123ffd1bf2a117d53a21bd
eugreg <daiaegregory@gmail.com>
2023-03-31 15:41:15 -0300

    adicao verbose name cor

--- garagem/models.py
+++ garagem/models.py
@@ -23,6 +23,9 @@ class Cor(models.Model):
     descricao = models.CharField(max_length=100)
     def __str__(self) :
         return self.descricao
+    
+    class Meta:
+        verbose_name_plural="Cor"
 
 class Veiculos(models.Model):
     modelo = models.CharField(max_length=100)
